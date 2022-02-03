024d5cc2c63e9b3ac6643bc9c1d4be8213948a07
Darrenfisherlol <darren.nov@gmail.com>
2022-02-02 18:16:48 -0600

    Created all methods except for asking the user to enter more orders

--- TaskThree/Program.cs
+++ TaskThree/Program.cs
@@ -1,9 +1,8 @@
 ﻿/*
  *Darren Fisher
  *MIS 3033
- * hw 2 task 3
+ *hw 2 task 3
  */
-
 
 
 /* To Do:
@@ -26,19 +25,139 @@ indicate that they do not.
  *
  */
 
+using System;
+using System.Collections.Generic;
+
 namespace TaskThree
 {
 
-    class program
+    class Receipt
     {
-        private double CogPrice;
-        private double GearPrice;
+        
+        public int CustomerID;
+        public int CogQuantity;
+        public int GearQuantity;
+        public DateTime SaleDate;
 
-        public static void Main(string[]args)
+        private double SalesTaxPercent = .089;
+        private double CogPrice = 79.99;
+        private double GearPrice = 250;
+
+        // null constructor
+        public Receipt()
         {
 
         }
 
+        // main constructor
+        public Receipt(int customerID, int numCogs, int numGears)
+        {
+            CustomerID = customerID;
+            CogQuantity = numCogs;
+            GearQuantity = numGears;
+            SaleDate = DateTime.Now;
+
+        }
+
+        /*
+        The CalculateTotal method will tabulate the final total for the sale.  To do this, 
+        you will need to call the CalculateNetAmount method and the CalculateTaxAmount method to 
+        get the net amount and the tax amount.  You will then add the tax amount from the net amount 
+        and return that value.
+         */
+        public double CalculateTotal()
+        {
+            double totalAmount = 0;
+            totalAmount = CalculateNetAmount() + CalculateTaxAmount();
+
+            return totalAmount;
+        }
+
+        /*
+        The PrintReceipt method will write to the console all the details about the sale in 
+        an easy to read format.  Not all on just one line, must include what the value represents (e.g. not 
+        just 5 but # of Cogs : 5).  Format all values appropriately. 
+         */
+        public void PrintReceipt()
+        {
+
+            Console.WriteLine("Customer ID: " + CustomerID);
+            Console.WriteLine("Cogs Quantity: " + CogQuantity);
+            Console.WriteLine("Gears Quantity: " + GearQuantity);
+            Console.WriteLine("Sale Date: " + SaleDate);
+            Console.WriteLine("Sales Tax Percent: " + SalesTaxPercent);
+            Console.WriteLine("Cogs Price" + CogPrice);
+            Console.WriteLine("Gears Price : " + GearPrice);
+        }
+
+        /*
+         The CalculateTaxAmount method will tabulate the total tax for the 
+         sale.  To do this, you will need to call the CalculateNetAmount and then multiple by your class 
+         (instance) variable, SalesTaxPercent.  You will then return the total tax for the sale (Note: this 
+         should be a positive value).
+         */
+        private double CalculateTaxAmount()
+        {
+            double taxAmount = 0; 
+            taxAmount = CalculateNetAmount() * SalesTaxPercent; 
+
+            return taxAmount;
+        }
+
+
+        /*
+         The CalculateNetAmount method will tabulate the net price of the 
+        sale.  You will need to figure out what markup percent we need to add to our base unit prices for 
+        the Cog and Gears based upon the number purchased.  Once you do this, the formula for to 
+        calculate the netAmount is:
+        netAmount = CogQuantity * Cog Price with markup + GearQuantity * Gear Price with 
+        markup
+        You will then return the net amount for the sale from the method.
+         */
+        private double CalculateNetAmount()
+        {
+            double netAmount = 0;
+
+            //12.5%. If quantity is > 10 or if the sum of q is = 16 
+            if (CogQuantity > 10 || GearQuantity > 10 || (CogQuantity + GearQuantity) == 16)
+            {
+                netAmount = CogQuantity * CogPrice * (CogPrice * .125) + GearQuantity * GearPrice * (GearPrice * .125);
+            }
+            // 15%
+            else
+            {
+                netAmount = CogQuantity * CogPrice * (CogPrice * .15) + GearQuantity * GearPrice * (GearPrice * .15);
+            }
+            return netAmount;
+        }
+    }
+
+    class program
+    {
+
+        public static void Main()
+        {
+            // constructor
+            List<Receipt> receiptList = new List<Receipt>();
+
+            // ask user for # cogs
+            Console.WriteLine("Enter the amount of cogs you want to buy");
+            int numCogs = Convert.ToInt32(Console.ReadLine());
+
+            // ask user for # gears
+            Console.WriteLine("Enter the amount of gears you want to buy");
+            int numGears = Convert.ToInt32(Console.ReadLine());
+
+            // ask user for id 
+            Console.WriteLine("Enter your Customer ID for this sale order");
+            int customerID = Convert.ToInt32(Console.ReadLine());
+
+            Receipt receiptOne = new Receipt(customerID, numCogs, numGears);
+            receiptList.Add(receiptOne);
+           
+
+            Console.WriteLine(receiptOne);
+        }
     }
 }
 
